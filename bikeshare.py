7552b4c00dcb0bb472917b08a48f603e4577636f
John Heath <john_heath@yahoo.com>
2020-05-12 17:19:41 -0600

    Added lower function to all questions asked of the user.
    Why not make it easy on the user for all questions asked so they don;t 
    need to worry about capitalization.

--- bikeshare.py
+++ bikeshare.py
@@ -28,8 +28,8 @@ def get_filters():
 
     # get user input for month (all, january, february, ... , june)
     while True:
-        month = input("\nWhat month do you want to filter by (January, February, March, April, May, June, or All? \n")
-        if month not in ('January', 'February', 'March', 'April', 'May', 'June', 'All'):
+        month = input("\nWhat month do you want to filter by (January, February, March, April, May, June, or All? \n").lower()
+        if month not in ('january', 'february', 'march', 'april', 'may', 'june', 'all'):
             print("Sorry, I didn't catch that. Try again.")
             continue
         else:
@@ -37,8 +37,8 @@ def get_filters():
 
     # get user input for day of week (all, monday, tuesday, ... sunday)
     while True:
-        day = input("\nWhat day do you want to filter by (Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday or All)?\n")
-        if day not in ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'All'):
+        day = input("\nWhat day do you want to filter by (Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday or All)?\n").lower()
+        if day not in ('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'all'):
             print("Sorry, I didn't catch that. Try again.")
             Continue
         else:
